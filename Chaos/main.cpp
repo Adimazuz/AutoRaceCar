3d046be07d3991df59b89537e64e4356e435f223
didemon2005 <dima_klt@yahoo.com>
2019-06-11 21:34:27 +0300

    added TcpServer lib

--- Chaos/main.cpp
+++ Chaos/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include "ITcpClient.h"
 
@@ -7,8 +8,26 @@ int main() {
 
 
     RaceCar car;
-    car.connect("132.",5560);
+//    car.connect("132.",5560);
     car.run();
 
     std::cout << " ok" << std::endl;
+
+
+    std::thread thread([&car]()
+    {
+        while(true)
+        {
+            char ch;
+            std::cin >> ch;
+            if(ch == 'q')
+            {
+                car._is_running = false;
+                break;
+            }
+        }
+    });
+
+    thread.join();
+    return 0;
 }
