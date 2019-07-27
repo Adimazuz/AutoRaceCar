46308dafd3f3aa4aef2fc4e0f17f30c94dcbcdce
Adi Mazuz <mazuz0992@gmail.com>
2019-07-27 19:26:09 +0300

    added bitcraze to arduino

---
+++ arduino/RaceCarArd/bitcraze.cpp
@@ -0,0 +1,53 @@
+#include "Bitcraze_PMW3901.h"
+#include <Wire.h>
+#include <VL53L0X.h>
+#include "bitcraze.h"
+#include "flow.h"
+
+
+
+	void bitcraze::setup()
+	{
+		Bitcraze_PMW3901 flow(6);
+		Serial.begin(9600);
+		if (!flow.begin()) {
+			Serial.println("Initialization of the flow sensor failed");
+			while(1) { }
+		}
+		// Initialize range sensor
+		Wire.begin();
+		_rangeSensor.init();
+		_rangeSensor.setTimeout(500);
+	}
+ 
+	void bitcraze::getData()
+	{
+		flow.readMotionCount(&_deltaX, &_deltaY);
+		_range = _rangeSensor.readRangeSingleMillimeters();
+		  Serial.print("X: ");
+		Serial.print(_deltaX);
+    //Serial.write(_deltaX);
+		Serial.print(", Y: ");
+		Serial.print(_deltaY);
+   // Serial.write(_deltaY);
+		Serial.print(", Range: ");
+		if (_range > 5000) {
+      Serial.write("N/A");
+			//Serial.print("N/A");
+		} else {
+			Serial.print(_range);
+      //Serial.Write(_range);
+		}
+		Serial.print("\n");
+
+   //send data
+   Flow data(_deltaX,_deltaY,_range);
+   char*  p_to_send = (char *) &data;
+   for( char c_Index = 0 ; c_Index < sizeof( Flow ) ; c_Index++)
+   {
+    Serial.write( p_to_send[ c_Index] );
+   }
+      Serial.print("\n");
+
+		delay(100);
+	}
---
+++ arduino/RaceCarArd/flow.h
@@ -0,0 +1,7 @@
+class Flow {
+  int deltaX;
+  int deltaY;
+  float range;
+  public:
+  Flow(int x,int y, int r): deltaX(x),deltaY(y),range(r){};
+};
