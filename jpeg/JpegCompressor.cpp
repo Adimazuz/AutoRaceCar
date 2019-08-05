460cd2078e73eac35f918143863261f508418812
asaf <asafanter@gmail.com>
2019-08-05 16:22:50 +0300

    add Jpeg

---
+++ jpeg/JpegCompressor.cpp
@@ -0,0 +1,90 @@
+#include "JpegCompressor.h"
+
+JpegCompressor::JpegCompressor() :
+    _width(640),
+    _height(480),
+    _format(Format::RGB),
+    _quality_percent(100),
+    _output(nullptr),
+    _handle(),
+    _compressed_size(0)
+{
+
+}
+
+JpegCompressor::JpegCompressor(const uint32 &width, const uint32 &height,
+                               const JpegCompressor::Format &format, const uint32 &quality_percent) :
+    _width(width),
+    _height(height),
+    _format(format),
+    _quality_percent(quality_percent),
+    _output(nullptr),
+    _compressed_size(0)
+{
+
+}
+
+void JpegCompressor::setParams(const uint32 &width, const uint32 &height,
+                               const JpegCompressor::Format &format, const uint32 &quality_percent)
+{
+    _width = width;
+    _height = height;
+    _format = format;
+    _quality_percent = quality_percent;
+}
+
+void JpegCompressor::compress(const uint8 *input)
+{
+    tjFree(_output);
+    _handle = tjInitCompress();
+
+    TJPF format = TJPF_RGB;
+    if(_format == Format::GREY_SCALE)
+    {
+        format = TJPF_GRAY;
+    }
+
+    tjCompress2(_handle, input, static_cast<int32>(_width), 0, static_cast<int32>(_height),
+                format, &_output, &_compressed_size, TJSAMP_444,
+                static_cast<int32>(_quality_percent), TJFLAG_FASTDCT);
+
+    tjDestroy(_handle);
+}
+
+uint32 JpegCompressor::formatToInt(const JpegCompressor::Format &format)
+{
+    if(format == Format::RGB)
+    {
+        return 3;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+
+//void compress(int quality, int width, int height, unsigned char *input, unsigned char **output,
+//              unsigned long *compressed_size)
+//{
+//    tjhandle _jpegCompressor = tjInitCompress();
+
+//    tjCompress2(_jpegCompressor, input, width, 0, height, TJPF_RGB,
+//              output, compressed_size, TJSAMP_444, quality,
+//              TJFLAG_FASTDCT);
+
+//    tjDestroy(_jpegCompressor);
+//}
+
+//void decompress(unsigned char *input, unsigned char *output, unsigned long size, int width, int height)
+//{
+//    int jpegSubsamp;
+
+//    tjhandle _jpegDecompressor = tjInitDecompress();
+
+//    tjDecompressHeader2(_jpegDecompressor, input, size, &width, &height, &jpegSubsamp);
+
+//    tjDecompress2(_jpegDecompressor, input, size, output, width, 0, height, TJPF_RGB, TJFLAG_FASTDCT);
+
+//    tjDestroy(_jpegDecompressor);
+//}
---
+++ jpeg/JpegDecompressor.h
@@ -0,0 +1,39 @@
+#ifndef JPEGDECOMPRESSOR_H
+#define JPEGDECOMPRESSOR_H
+
+#include "turbojpeg.h"
+
+using uint32 = unsigned int;
+using uint8 = unsigned char;
+using uint64 = unsigned long;
+using int32 = int;
+
+class JpegDecompressor
+{
+public:
+
+    enum class Format
+    {
+        RGB,
+        GREY_SCALE
+    };
+
+    JpegDecompressor();
+    JpegDecompressor(const uint32 &width, const uint32 &height,
+                     const JpegDecompressor::Format &format);
+    void decompress(uint8 *input, const uint64 &compressed_size);
+    uint8 *getOutput() {return _output;}
+    ~JpegDecompressor() {delete[] _output;}
+
+private: //methods
+    uint32 formatToInt(const Format &format);
+
+private: //members
+    uint32 _width;
+    uint32 _height;
+    Format _format;
+    uint8 *_output;
+    tjhandle _handle;
+};
+
+#endif // JPEGDECOMPRESSOR_H
