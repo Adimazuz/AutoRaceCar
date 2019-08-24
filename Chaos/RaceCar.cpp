07eb1c2636816839a0c9c1868b57325a819bdc16
Dima <dima_klt@yahoo.com>
2019-08-24 12:05:20 +0300

    added DepthPacket Func. & documintation in Chaos Code

--- Chaos/RaceCar.cpp
+++ Chaos/RaceCar.cpp
@@ -124,11 +124,11 @@ RaceCar &RaceCar::run()
             std::cout << "Camera setuped" <<std::endl;
             _camera.startCamera();
             std::cout << "Camera started" <<std::endl;
-            _camera_thread = std::make_shared<std::thread>(&RaceCar::getCameraOutput,this);
+            _camera_thread = std::make_shared<std::thread>(&RaceCar::getCameraOutputAndSendToRemote,this);
     }
     if(_is_motor_control_connected && _tcp_server->isBind()){
             std::cout << "connected to Motor Control" <<std::endl;
-            _carcontrol_thread = std::make_shared<std::thread>(&RaceCar::arduinoCommunications,this);
+            _carcontrol_thread = std::make_shared<std::thread>(&RaceCar::getCarControlCommands,this);
     }
     if(_is_bitcraze_connected){
         std::cout << "connected to BitCraze" <<std::endl;
@@ -138,7 +138,7 @@ RaceCar &RaceCar::run()
 }
 
 
-RaceCar &RaceCar::parseCmdString(const char cmd)
+RaceCar &RaceCar::parseAndSendCmd(const char cmd)
 {
 
     switch (cmd) {
@@ -172,7 +172,7 @@ RaceCar &RaceCar::parseCmdString(const char cmd)
 
 }
 
-RaceCar &RaceCar::arduinoCommunications()
+RaceCar &RaceCar::getCarControlCommands()
 {
     std::cout << "waiting for connection" << std::endl;
 
@@ -183,7 +183,7 @@ RaceCar &RaceCar::arduinoCommunications()
         {
             char cmd = ' ';
             _tcp_server->receive(_socket,&cmd, 1);
-            parseCmdString(cmd);
+            parseAndSendCmd(cmd);
 
         }
 
@@ -203,7 +203,7 @@ RaceCar &RaceCar::arduinoCommunications()
     _motor_control->stop();
     return *this;
 }
-//TODO to depth functions
+
 //TODO set params for different ressolutions
 //TODO JpegDecompressor implement setParams
 //TODO remoteControl showImage according to number_of_components
@@ -238,7 +238,40 @@ Chaos::header RaceCar::buildColorHeader(){
     return header;
 }
 
-RaceCar &RaceCar::getCameraOutput()
+Chaos::DepthPacket RaceCar::buildDepthPacket(const Camera::DepthImage &image){
+    
+    Chaos::DepthPacket packet = {};
+    
+    packet.accel_data = _camera.getAccelData();
+    packet.euler_angl = _camera.getEulerAngels();
+    
+    _flow_mtx.lock();
+    packet.flow_data = _flow_data;
+    _flow_mtx.unlock();
+    
+    packet.image.frame_num = image.frame_num;
+    packet.image.height = image.height;
+    packet.image.width = image.width;
+    packet.image.size = image.size;
+    packet.image.timestamp_ms = image.timestamp_ms;
+    packet.image.depth_scale = image.depth_scale;
+    
+    _jpeg_comp.compress(image.data);
+    packet.image.compressed_size = _jpeg_comp.getCompressedSize();
+    packet.image.compresed_data = _jpeg_comp.getOutput();
+    
+    return packet;
+}
+
+Chaos::header RaceCar::buildDepthHeader(){
+    Chaos::header header = {};
+    header.type_code = Chaos::DEPTH_HEADER;
+    header.total_size = sizeof(Chaos::DepthPacket)-sizeof(Chaos::DepthImage::compresed_data);
+    return header;
+}
+
+
+RaceCar &RaceCar::getCameraOutputAndSendToRemote()
 {
     std::cout << "enter Camera thread" <<std::endl;
     while (_is_running && _tcp_client->isConnected())
@@ -246,14 +279,21 @@ RaceCar &RaceCar::getCameraOutput()
 //        std::cout << _is_running <<std::endl;
         _camera.captureFrame();
         Camera::ColorImage image=_camera.getColorImage();
-
         Chaos::ColorPacket packet = buildColorPacket(image);
         Chaos::header header = buildColorHeader();
-
+        
         _tcp_client->send(reinterpret_cast<char*>(&header), sizeof(header));
         _tcp_client->send(reinterpret_cast<char*>(&packet), header.total_size);
         _tcp_client->send(reinterpret_cast<char*>(packet.image.compresed_data), packet.image.compressed_size);
-
+    
+        //TODO check flow for depth image:
+//        Camera::DepthImage d_image=_camera.getDepthImage();
+//        Chaos::DepthPacket d_packet = buildDepthPacket(d_image);
+//        Chaos::header d_header = buildDepthHeader();
+//        _tcp_client->send(reinterpret_cast<char*>(&d_header), sizeof(d_header));
+//        _tcp_client->send(reinterpret_cast<char*>(&d_packet), d_header.total_size);
+//        _tcp_client->send(reinterpret_cast<char*>(d_packet.image.compresed_data), d_packet.image.compressed_size);
+    
     }
     std::cout << "Camera thread finished" <<std::endl;
     return *this;
@@ -278,33 +318,5 @@ RaceCar &RaceCar::getBitCrazeOutput()
     std::cout << "BitCraze thread finished" << std::endl;
 }
 
-//RaceCar  &RaceCar::sendFlowOutput(Flow data)
-//{
-//    char* ptr = (char*) &data;
-//    _tcp_server->send(_socket,ptr,sizeof(Flow));
-//}
-//static void splitString(const string &str, std::vector<string> &output)
-//{
-//    string::size_type start = 0; // Where to start
-//    string::size_type last = str.find_first_of(" "); // Finds the first space
-
-//    // npos means that the find_first_of wasn't able to find what it was looking for
-//    // in this case it means it couldn't find another space so we are at the end of the
-//    // words in the string.
-//    while (last != string::npos)
-//    {
-//        // If last is greater then start we have a word ready
-//        if (last > start)
-//        {
-//            output.push_back(str.substr(start, last - start)); // Puts the word into a vector look into how the method substr() works
-//        }
-
-//        start = ++last; // Reset start to the first character of the next word
-//        last = str.find_first_of(" ", last); // This means find the first space and we start searching at the first character of the next word
-//    }
-
-//    // This will pickup the last word in the file since it won't be added to the vector inside our loop
-//    output.push_back(str.substr(start));
-//}
 
 
