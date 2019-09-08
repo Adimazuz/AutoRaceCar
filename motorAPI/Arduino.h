6b5fc889ed38242fde9cd3d547459bf395530a20
Adi Mazuz <mazuz0992@gmail.com>
2019-09-08 19:48:16 +0300

    changed reslution

--- motorAPI/Arduino.h
+++ motorAPI/Arduino.h
@@ -72,8 +72,8 @@ private:
     std::shared_ptr<ISerial> m_serial;
     int m_current_speed=0;
     int m_current_angle=90;
-    static constexpr int START_SPEED = 8;
-    static constexpr int TOP_SPEED = 12;
+    static constexpr int START_SPEED = 70;
+    static constexpr int TOP_SPEED = 500;
     static constexpr int MIN_ANGLE = 30;
     static constexpr int MAX_ANGLE = 150;
     void sendDriveCommand();
